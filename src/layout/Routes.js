9d7a2c216cf0ce32ac6cf0efa312c42b4acf3032
Jeremiah Adsit <jeremiah.adsit@gmail.com>
2022-04-10 15:14:08 -0600

    add home page and path

--- src/layout/Routes.js
+++ src/layout/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Home from "../components/Home";
 import About from "../components/About";
 import NotFound from "../components/NotFound";
 
@@ -8,6 +9,10 @@ function Routes() {
         <Switch>
             <Route exact={true} path="/about">
                 <About />
+            </Route>
+
+            <Route exact={true} path="/">
+                <Home />
             </Route>
 
             <Route>
