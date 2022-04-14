aee3a8958d3837e03e020588b3467739312eb04b
Jeremiah Adsit <jeremiah.adsit@gmail.com>
2022-04-14 16:51:25 -0600

    added second example project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,10 +9,19 @@ function Projects() {
         linkText: "This is a link."
     };
 
+    let exampleTwo = {
+        title: "Example",
+        description: "This is an imaginary thing.",
+        html: "https://www.duckduckgo.com",
+        linkText: "This goes somewhere."
+    }
+
     return (
         <div>
             <ProjectCard 
             project={exampleProject} />
+            <ProjectCard
+            project={exampleTwo} />
         </div>
     );
 }
